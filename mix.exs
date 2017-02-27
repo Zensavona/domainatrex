3fad3038eb3297b3e1deb16ea809eb8ed80f5f5f
Zen Savona <tedelex06@gmail.com>
2017-02-27 14:34:18 +1100

    travis stuff

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Domainatrex.Mixfile do
   def project do
     [app: :domainatrex,
      version: "1.0.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.3",
      test_coverage: [tool: ExCoveralls],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
