cab0dd41128aafeee19422c1bff22ad15afc2aa0
Zen Savona <tedelex06@gmail.com>
2017-03-31 14:27:15 +1100

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Domainatrex.Mixfile do
 
   def project do
     [app: :domainatrex,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.3",
      test_coverage: [tool: ExCoveralls],
      build_embedded: Mix.env == :prod,
