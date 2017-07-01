cfbff44a58755808380289798645600201b5caad
Zen Savona <tedelex06@gmail.com>
2017-07-02 01:18:52 +1000

    s3.amazonaws.com test

--- test/domainatrex_test.exs
+++ test/domainatrex_test.exs
@@ -13,4 +13,9 @@ defmodule DomainatrexTest do
   test "subdomains" do
     assert Domainatrex.parse("my.blog.zen.id.au") == {:ok, %{domain: "zen", subdomain: "my.blog", tld: "id.au"}}
   end
+
+  test "s3" do
+   domain = "s3.amazonaws.com"
+   assert Domainatrex.parse(domain) == {:ok, %{domain: "amazonaws", subdomain: "s3", tld: "com"}}
+ end
 end
